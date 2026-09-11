f6f90b397fb871d832c55e8b6555a9501423b11b
Phong Lien <phonglien2211@gmail.com>
2026-09-11 16:09:06 -0400

    Implemented a loading screen with animated text and progress bar

---
+++ frontend/src/app/components/LoadingScreen.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {useEffect, useState} from "react";
+import React from "react";
+
+type LoadingScreenProps = {
+    onComplete: () => void;
+};
+
+export const LoadingScreen = ({ onComplete }: LoadingScreenProps) => {
+    const [text, setText] = React.useState("");
+    const fullText = "Welcome to Life Dashboard";
+
+    useEffect(() => {
+        let index = 0
+        const interval = setInterval(() => {
+            setText((fullText.substring(0, index)));
+            index++;
+            if (index > fullText.length) {
+                clearInterval(interval);
+
+                setTimeout(() => {
+                    onComplete();
+                }, 1000);
+            }
+        }, 100)
+        
+        return () => clearInterval(interval);
+    }, [onComplete]);
+
+    return <div className="fixed inset-0 z-50 text-gray-100 flex flex-col items-center justify-center">
+        <div className="mb-4 text-4xl font-mono font-bold">{text}<span className="animate-blink ml-1">| </span></div>
+        <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+            <div className="animate-loading w-[40%] h-full bg-blue-500"></div>
+        </div>
+    </div>
+}
